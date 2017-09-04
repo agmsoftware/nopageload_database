f33c0f8bf5e7d915b1bfde9ff02c7bf4b5ea2f3e
Dendy B. Sulistyo <dendybsulistyo@gmail.com>
2017-09-04 13:39:02 +0700

    update
    update

--- application/controllers/Proses.php
+++ application/controllers/Proses.php
@@ -8,15 +8,10 @@ class Proses extends CI_Controller {
 	{
 
 
-
 	$nama_mhs	=$_POST['nama_mhs'];
 	$no_mhs		=$_POST['no_mhs'];
 	$matkul_mhs	=$_POST['matkul_mhs'];
 
-	//$nama_mhs				= $this->input->post('nama_mhs');
-	//$no_mhs 				= $this->input->post('no_mhs');
-	//$matkul_mhs				= $this->input->post('matkul_mhs')
-	
 
 	// load model
 	$this->load->model('m_mhs');
@@ -27,16 +22,9 @@ class Proses extends CI_Controller {
 	$data['no_mhs']		=$_POST['no_mhs'];
 	$data['matkul_mhs']	=$_POST['matkul_mhs'];
 
-		//$stmt = $DBcon->prepare("INSERT INTO student(student_name,student_roll_no,student_class) VALUES(:student_name, :student_roll_no,:student_class)");
 
-		// hasil entrys
-		//$nama_mhs				= $this->input->post('nama_mhs');
-		//$no_mhs 				= $this->input->post('no_mhs');
-		//$matkul_mhs				= $this->input->post('matkul_mhs')
-
-
-		// tampilkan view
-		$this->load->view('show', $data);
+	// tampilkan view
+	$this->load->view('show', $data);
 		
 	}
 
